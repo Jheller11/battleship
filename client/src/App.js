76c8664fc634ca6dc9a4e4dfe49d3ffa7a1968df
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-05-13 20:27:54 -0400

    Install and config react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
+import Landing from './views/Landing/Landing'
 
 function App() {
   return (
     <div className="App">
       <h1>Battleship</h1>
+      <Switch>
+        <Route path="/" exact component={Landing} />
+      </Switch>
     </div>
   )
 }
---
+++ client/src/views/Landing/Landing.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Landing.module.css'
+
+const Landing = () => {
+  return <div>THis is the landing page</div>
+}
+
+export default Landing
