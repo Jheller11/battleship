7ba7c581cd4b3973cab5ff5da5280c2a97a6b5c1
Jeffrey Heller <heller.jeffrey@gmail.com>
2019-05-15 12:10:39 -0400

    CLIENT  - set up grid labeling/layout

--- client/src/components/PlayerBoard/PlayerBoard.js
+++ client/src/components/PlayerBoard/PlayerBoard.js
@@ -7,10 +7,37 @@ const PlayerBoard = props => {
   return (
     <div>
       <h1>Player Ship Arrangement</h1>
-      <div className={styles.grid}>
-        {props.board.map((square, i) => {
-          return <Squares.PlayerSquare key={i} data={square} />
-        })}
+      <div className={styles.numberLine}>
+        <span />
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span>10</span>
+      </div>
+      <div className={styles.flex}>
+        <div className={styles.letterColumn}>
+          <span>A</span>
+          <span>B</span>
+          <span>C</span>
+          <span>D</span>
+          <span>E</span>
+          <span>F</span>
+          <span>G</span>
+          <span>H</span>
+          <span>I</span>
+          <span>J</span>
+        </div>
+        <div className={styles.grid}>
+          {props.board.map((square, i) => {
+            return <Squares.PlayerSquare key={i} data={square} />
+          })}
+        </div>
       </div>
     </div>
   )
--- client/src/components/ShotBoard/ShotBoard.js
+++ client/src/components/ShotBoard/ShotBoard.js
@@ -16,7 +16,34 @@ const ShotBoard = props => {
   return (
     <div>
       <h1>Shot Board</h1>
-      <div className={styles.grid}>{squares}</div>
+      <div className={styles.numberLine}>
+        <span />
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span>10</span>
+      </div>
+      <div className={styles.flex}>
+        <div className={styles.letterColumn}>
+          <span>A</span>
+          <span>B</span>
+          <span>C</span>
+          <span>D</span>
+          <span>E</span>
+          <span>F</span>
+          <span>G</span>
+          <span>H</span>
+          <span>I</span>
+          <span>J</span>
+        </div>
+        <div className={styles.grid}>{squares}</div>
+      </div>
     </div>
   )
 }
